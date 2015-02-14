bcee5866138a16124dff0abc0b4602b082b8c919
Minh Tri Nguyen <minhtringuyen001@hotmail.com>
2015-02-14 06:28:29 +0000

    Update application.yml and development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  config.action_mailer.perform_deliveries = true
 end
